48c5e016aaf2aecba6448677c25754c360fb642e
Slumrag <mrgreenthecristal@yandex.ru>
2023-11-10 17:19:18 +0300

    Updates

---
+++ precache-manifest.b200ffafe692a7ddd63260a620528347.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20463812dd8aa806d4cc60aa8ebc04c5",
+    "url": "/typescriptTask/index.html"
+  },
+  {
+    "revision": "9d3d30dd0224015896a8",
+    "url": "/typescriptTask/static/css/main.9fd8d12a.chunk.css"
+  },
+  {
+    "revision": "234090833f731ebe2bd3",
+    "url": "/typescriptTask/static/js/2.1b3cdf7e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/typescriptTask/static/js/2.1b3cdf7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3d30dd0224015896a8",
+    "url": "/typescriptTask/static/js/main.94886136.chunk.js"
+  },
+  {
+    "revision": "cda5aa9ef40f8dc03b45",
+    "url": "/typescriptTask/static/js/runtime-main.bc32402b.js"
+  },
+  {
+    "revision": "98878a7fb6c4d11f006f5f0a48816bae",
+    "url": "/typescriptTask/static/media/Kontur-Iconic-eco-20px.98878a7f.woff2"
+  },
+  {
+    "revision": "b811ce6cc66ff93ba6ba6dd225b475df",
+    "url": "/typescriptTask/static/media/Kontur-Iconic-eco-20px.b811ce6c.eot"
+  },
+  {
+    "revision": "cc8e65cb2d3a893285466730d7b4f022",
+    "url": "/typescriptTask/static/media/Kontur-Iconic-eco-20px.cc8e65cb.woff"
+  },
+  {
+    "revision": "7d850235c4b0a12aed1c0d83e489acd5",
+    "url": "/typescriptTask/static/media/fallback_circle.7d850235.png"
+  },
+  {
+    "revision": "7890347bd63a549c5164f53487b8031f",
+    "url": "/typescriptTask/static/media/fallback_circle_big.7890347b.png"
+  },
+  {
+    "revision": "dae3133efdcd89ac9e4d3335d2675b1d",
+    "url": "/typescriptTask/static/media/fallback_circle_normal.dae3133e.png"
+  },
+  {
+    "revision": "7b359e7d9365a987d17524f81fc66fb5",
+    "url": "/typescriptTask/static/media/font.7b359e7d.woff2"
+  },
+  {
+    "revision": "dc62e0fc93a9b5469860b6a22338562d",
+    "url": "/typescriptTask/static/media/font.dc62e0fc.eot"
+  },
+  {
+    "revision": "f01e8d1506e8cb4cbb1f1c39dab9dab9",
+    "url": "/typescriptTask/static/media/font.f01e8d15.woff"
+  }
+]);
